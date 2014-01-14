2aca205db2fa1c1c7e98db7e0e75468c2a527b44
lordtoede <sora_no_koto@hotmail.com>
2014-01-14 16:01:45 +0000

    1

---
+++ src/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ZebenzuíJosé
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
